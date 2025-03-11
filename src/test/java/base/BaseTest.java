63b272112ff92929e14de3835e3d22453b3f8d5c
Roberto Gomez <roberto.gomez02@globant.com>
2025-03-11 11:23:32 -0500

    feat: initial test configuration

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,39 @@
+package base;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.net.URI;
+
+public class BaseTest {
+
+    protected static AndroidDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = createAndroidDriver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    private AndroidDriver createAndroidDriver() {
+        UiAutomator2Options options = new UiAutomator2Options()
+                .setApp("android.wdio.native.app.v1.0.8.apk")
+                .setDeviceName("Prueba Appium API 35")
+                .setPlatformVersion("15")
+                .setAppActivity("com.wdiodemoapp.MainActivity");
+        try {
+            URI uri = new URI("http://127.0.0.1:4723");
+            return new AndroidDriver(uri.toURL(), options);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/test/FirstTest.java
@@ -0,0 +1,9 @@
+package test;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+
+public class FirstTest extends BaseTest {
+    @Test
+    public void test(){}
+}
