6979ed393d3bcde8efae2f4da663bb9162fceff0
Roberto Gomez <roberto.gomez02@globant.com>
2025-03-11 14:35:58 -0500

    feat: Navigation: Webview test

--- src/test/java/screen/WebviewScreen.java
+++ src/test/java/screen/WebviewScreen.java
@@ -3,11 +3,7 @@ package screen;
 import base.BaseScreen;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebviewScreen extends BaseScreen {
 
